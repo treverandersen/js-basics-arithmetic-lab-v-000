c88b998425ca31c7e6ab028953315f79bd2f3a13
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-22 19:23:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.floor(Math.random() * 20) + 1;
 const randomInteger = Math.floor(randomNumber);
-const randomUserID = (randomInteger * 1000000020-1000000001)+1;
+const randomUserID = randomInteger * 1000000020 - 1000000001 +1;
